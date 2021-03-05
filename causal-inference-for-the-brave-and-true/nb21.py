1d927dfc751cc97c142fc09f097ccba62f8edb35
Matheus Facure <matheusfacure01@gmail.com>
2021-03-05 08:00:32 -0300

    Chapter 21 (#32)
    * make intro to 20 more dramatic
    
    * replace customers by day
    
    * add chapter 21
    
    * finish draft
    
    * simplify othogonal implementation
    
    * review chatper
    
    * memefy
    
    * fix toc

---
+++ causal-inference-for-the-brave-and-true/nb21.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+from toolz import merge, curry
+from sklearn.preprocessing import LabelEncoder
+
+@curry
+def elast(data, y, t):
+        return (np.sum((data[t] - data[t].mean())*(data[y] - data[y].mean())) /
+                np.sum((data[t] - data[t].mean())**2))
+    
+    
+def elast_ci(df, y, t, z=1.96):
+    n = df.shape[0]
+    t_bar = df[t].mean()
+    beta1 = elast(df, y, t)
+    beta0 = df[y].mean() - beta1 * t_bar
+    e = df[y] - (beta0 + beta1*df[t])
+    se = np.sqrt(((1/(n-2))*np.sum(e**2))/np.sum((df[t]-t_bar)**2))
+    return np.array([beta1 - z*se, beta1 + z*se])
+
+
+def cumulative_elast_curve_ci(dataset, prediction, y, t, min_periods=30, steps=100):
+    size = dataset.shape[0]
+    ordered_df = dataset.sort_values(prediction, ascending=False).reset_index(drop=True)
+    n_rows = list(range(min_periods, size, size // steps)) + [size]
+    
+    return np.array([elast_ci(ordered_df.head(rows), y, t)  for rows in n_rows])
+
+
+def cumulative_gain_ci(dataset, prediction, y, t, min_periods=30, steps=100):
+    size = dataset.shape[0]
+    ordered_df = dataset.sort_values(prediction, ascending=False).reset_index(drop=True)
+    n_rows = list(range(min_periods, size, size // steps)) + [size]
+    return np.array([elast_ci(ordered_df.head(rows), y, t) * (rows/size) for rows in n_rows])
